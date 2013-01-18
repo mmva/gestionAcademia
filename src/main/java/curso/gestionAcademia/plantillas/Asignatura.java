5771c8171eccf5a015c3cc064412a2cb1adaaab0
Mari Mar Villarte Aznar <marimar.villarte@gmail.com>
2013-01-18 10:44:39 +0100

    Adaptacion plantilla clase

--- src/main/java/curso/gestionAcademia/plantillas/Asignatura.java
+++ src/main/java/curso/gestionAcademia/plantillas/Asignatura.java
@@ -1,5 +1,6 @@
 package curso.gestionAcademia.plantillas;
 
+import curso.gestionAcademia.util.Constantes;
 import curso.gestionAcademia.util.Secuencial;
 
 public class Asignatura {
@@ -12,13 +13,17 @@ public class Asignatura {
     // Constructor
     public Asignatura(String nombre, String tipo, int horasSemana){
         this.nombre = nombre;
-        this.tipo = tipo;
+        this.tipo = (tipo.equals(Constantes.TIP_INF) || tipo.equals(Constantes.TIP_IDI) )? tipo : Constantes.TIP_GEN;
         this.horasSemana = horasSemana;
         
         this.idAsignatura = Secuencial.getSecuencialAsignatura(tipo);
     }
 
     // Funciones para la recuperacion de la informacion del objeto
+    public long getIdAsignatura() {
+        return idAsignatura;
+    }
+   
     public String getNombre() {
         return nombre;
     }
